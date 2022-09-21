6372f7a78aa1041c4adbb920b22599d5f0df3228
thumm <thumm@LAPTOP-FODUFJRQ>
2022-09-21 16:39:45 +0530

    Third commit

--- SpringMVCPro1/src/main/java/com/tcs/controller/TestController.java
+++ SpringMVCPro1/src/main/java/com/tcs/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.tcs.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,6 +12,11 @@ public class TestController {
 	@RequestMapping("/info")
 	public String info()
 	{	return "Ratan sir is great....";
+	}
+	
+	@RequestMapping("/show/{name}")
+	public String show(@PathVariable String name)
+	{	return "Who is the best teacher...."+name;
 	}
 	
 }
