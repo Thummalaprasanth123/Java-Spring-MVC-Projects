52f1e7b1695f1906cf4064bcd26d8f6050a9578d
thumm <thumm@LAPTOP-FODUFJRQ>
2022-09-21 16:07:38 +0530

    First Commit

--- SpringMVCPro2/src/main/java/com/tcs/controller/ViewController.java
+++ SpringMVCPro2/src/main/java/com/tcs/controller/ViewController.java
@@ -2,15 +2,12 @@ package com.tcs.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-//@RestController
 @Controller
-@ResponseBody
-public class TestController {
+public class ViewController {
 	@RequestMapping("/info")
 	public String info()
-	{	return "Ratan sir is great....";
+	{	return "Success";
 	}
 	
 }
---
+++ SpringMVCPro2/src/main/webapp/WEB-INF/views/Success.jsp
@@ -0,0 +1,8 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<body>
+	<h1>Ratan sir is good in teaching</h1>
+</body>
+</html>
